a9018e055162e50feee6a4ee8da9fe25f9078297
Jungyeon <yeonns2@gmail.com>
2021-09-15 22:10:32 +0900

    feat : 유저 조회 API 추가

--- src/app/User/userDao.js
+++ src/app/User/userDao.js
@@ -1,7 +1,7 @@
 // 모든 유저 조회
 async function selectUser(connection) {
     const selectUserListQuery = `
-                  SELECT email, nickname 
+                  SELECT email, name 
                   FROM UserInfo;
                   `;
     const [userRows] = await connection.query(selectUserListQuery);
@@ -11,7 +11,7 @@ async function selectUser(connection) {
 // 이메일로 회원 조회
 async function selectUserEmail(connection, email) {
     const selectUserEmailQuery = `
-                  SELECT email, nickname 
+                  SELECT email, name 
                   FROM UserInfo 
                   WHERE email = ?;
                   `;
@@ -22,7 +22,7 @@ async function selectUserEmail(connection, email) {
 // userId 회원 조회
 async function selectUserId(connection, userId) {
     const selectUserIdQuery = `
-                   SELECT id, email, nickname 
+                   SELECT id, email, name 
                    FROM UserInfo 
                    WHERE id = ?;
                    `;
@@ -33,7 +33,7 @@ async function selectUserId(connection, userId) {
 // 유저 생성
 async function insertUserInfo(connection, insertUserInfoParams) {
     const insertUserInfoQuery = `
-          INSERT INTO UserInfo(email, password, nickname)
+          INSERT INTO UserInfo(email, password, name)
           VALUES (?, ?, ?);
       `;
     const insertUserInfoRow = await connection.query(
@@ -47,7 +47,7 @@ async function insertUserInfo(connection, insertUserInfoParams) {
 // 패스워드 체크
 async function selectUserPassword(connection, selectUserPasswordParams) {
     const selectUserPasswordQuery = `
-          SELECT email, nickname, password
+          SELECT email, name, password
           FROM UserInfo 
           WHERE email = ? AND password = ?;`;
     const selectUserPasswordRow = await connection.query(
@@ -74,7 +74,7 @@ async function selectUserAccount(connection, email) {
 async function updateUserInfo(connection, id, nickname) {
     const updateUserQuery = `
     UPDATE UserInfo 
-    SET nickname = ?
+    SET name = ?
     WHERE id = ?;`;
     const updateUserRow = await connection.query(updateUserQuery, [nickname, id]);
     return updateUserRow[0];
