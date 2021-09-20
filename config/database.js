9c92be74870affcf79c215790f64d5376ef01842
yeonns2 <yeonns2@gmail.com>
2021-09-20 16:10:07 +0900

    feat : AWS RDS 연동

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ const { logger } = require("./winston");
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
+    host: 'findit-rds.cfb8qbs4q57y.us-east-2.rds.amazonaws.com',
+    user: 'admin',
     port: "3306",
-    password: '',
-    database: 'test',
+    password: 'findit12#',
+    database: 'findit',
 });
 
 module.exports = {
