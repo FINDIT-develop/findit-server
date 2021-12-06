1d4ad5e2e133e023e4249a946e7cb03b315b0c44
Jungyeon <yeonns2@gmail.com>
2021-12-06 16:42:10 +0900

    feat : 제품 전체 조회 API

---
+++ src/app/Product/productController.js
@@ -0,0 +1,22 @@
+const jwtMiddleware = require("../../../config/jwtMiddleware");
+const productProvider = require("../../app/Product/productProvider");
+const productService = require("../../app/Product/productService");
+const baseResponse = require("../../../config/baseResponseStatus");
+const secret_config = require("../../../config//secret");
+const { response, errResponse } = require("../../../config/response");
+const request = require("request");
+const crypto = require("crypto");
+const cache = require("memory-cache");
+const jwt = require("jsonwebtoken");
+
+
+/**
+ * API No. 제품 전체 조회 API
+ * [POST] /app/products
+ */
+exports.getProducts = async function(req, res) {
+
+    const productsResult = await productProvider.retrieveProduct();
+
+    return res.send(response(baseResponse.SUCCESS, productsResult));
+}
